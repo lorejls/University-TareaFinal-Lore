ff96b3778ee5904d1df01dd85324fc24f5be80d5
lorejls <lorenalangone@gmail.com>
2024-06-20 15:56:58 +0200

    actualización

--- cypress/e2e/step_definitions/landing.cy.js
+++ cypress/e2e/step_definitions/landing.cy.js
@@ -7,10 +7,10 @@ Given('I visit the OpenCart website', ()=> {
 When("I navigate to the homepage", ()=> {
 })
 
-Then('the URL should be', ()=> {
-  cy.url().should("contain", "http://opencart.abstracta.us/");
+Then('the URL should be {string}', ()=> {
+  cy.url().should("eq", "http://opencart.abstracta.us/");
 })
 
-Then('the page title should be', ()=> {
+Then('the page title should be {string}', ()=> {
   cy.title().should("eq", "Your Store");
 })
--- cypress/e2e/step_definitions/viewAndEditCart.cy.js
+++ cypress/e2e/step_definitions/viewAndEditCart.cy.js
@@ -13,69 +13,70 @@ Given('the customer has added products to the cart', () => {
 
   When('the customer navigates to the cart page', () => {
     cy.get('#cart-total').click();
-    cy.get('#cart > ul > li > p > a').click();
+    cy.get('#cart >ul');
   });
-  
+
+ 
+
   Then('they should see a list of added products with their names, images, prices, quantities, and subtotal', () => {
-    cy.get('.table-bordered tbody tr').should('have.length', 2);
-  
-    cy.get('.table-bordered tbody tr').eq(0).within(() => {
-      cy.get('.text-left').eq(1).should('contain', 'MacBook'); 
+    cy.get('ul >li >table >tbody>tr').should('have.length', 2);
+    
+    cy.get('ul >li >table >tbody>tr').eq(0).within(() => {
+      cy.get('.text-left').should('contain','iPhone'); 
       cy.get('img').should('be.visible');
-      cy.get('.text-right').eq(0).should('contain', '$602.00'); 
-      cy.get('input').should('have.value', '1');
-      cy.get('.text-right').eq(1).should('contain', '$602.00'); 
+      cy.get('.text-right').eq(0).should('contain', '1'); 
+      cy.get('.text-right').eq(1).should('contain', '$123.20'); 
     });
   
-    cy.get('.table-bordered tbody tr').eq(1).within(() => {
-      cy.get('.text-left').eq(1).should('contain', 'iPhone'); 
+    cy.get('ul >li >table >tbody>tr').eq(1).within(() => {
+      cy.get('.text-left').should('contain', 'MacBook'); 
       cy.get('img').should('be.visible');
-      cy.get('.text-right').eq(0).should('contain', '$123.20');
-      cy.get('input').should('have.value', '1');
-      cy.get('.text-right').eq(1).should('contain', '$123.20');
+      cy.get('.text-right').eq(0).should('contain', '1');
+      cy.get('.text-right').eq(1).should('contain', '$602.00');
     });
   
     cy.get('.table-bordered tfoot .text-right').last().should('contain', '$725.20'); 
+     // #cart >ul  > li > div >p > [href*="80"]
   });
   
   Given('the customer is on the cart page', () => {
     cy.get('#cart-total').click();
-    cy.get('#cart > ul > li > p > a').click();
+    cy.get('#cart >ul  > li > div >p > [href*="80"]').click();
   });
   
-  When('the customer removes a product from the cart', () => {
-    cy.get('.table-bordered tbody tr').first().within(() => {
-      cy.get('.btn-danger').click();
-    });
-  });
+  // When('the customer removes a product from the cart', () => {
+  //   cy.get('.table-bordered tbody tr').first().within(() => {
+  //     cy.get('.btn-danger').click();
+  //   });
+  // });
   
-  Then('the product should disappear from the list of products in the cart', () => {
-    cy.get('.table-bordered tbody tr').should('have.length', 1);
-  });
+  // Then('the product should disappear from the list of products in the cart', () => {
+  //   cy.get('.table-bordered tbody tr').should('have.length', 1);
+  // });
   
-  Then('the updated total should reflect the removal of the product', () => {
-    cy.get('.table-bordered tfoot .text-right').last().should('contain', '$123.20'); 
-  });
+  // Then('the updated total should reflect the removal of the product', () => {
+  //   cy.get('.table-bordered tfoot .text-right').last().should('contain', '$123.20'); 
+  // });
   
-  When('the customer updates the quantity of a product', () => {
-    cy.get('.table-bordered tbody tr').first().within(() => {
-      cy.get('input').clear().type('3'); 
-      cy.get('.btn-primary').click(); 
-    });
-  });
+  // When('the customer updates the quantity of a product', () => {
+  //   cy.get('.table-bordered tbody tr').first().within(() => {
+  //     cy.get('input').clear().type('3'); 
+  //     cy.get('.btn-primary').click(); 
+  //   });
+  // });
   
-  Then('the quantity of the product should be updated in the cart list', () => {
-    cy.get('.table-bordered tbody tr').first().within(() => {
-      cy.get('input').should('have.value', '3');
-    });
-  });
+  // Then('the quantity of the product should be updated in the cart list', () => {
+  //   cy.get('.table-bordered tbody tr').first().within(() => {
+  //     cy.get('input').should('have.value', '3');
+  //   });
+  // });
   
-  Then('the subtotal of the product should be updated according to the new quantity', () => {
-    cy.get('.table-bordered tbody tr').first().within(() => {
-      cy.get('.text-right').eq(1).should('contain', '$1806.00'); 
-    });
-  });
+  // Then('the subtotal of the product should be updated according to the new quantity', () => {
+  //   cy.get('.table-bordered tbody tr').first().within(() => {
+  //     cy.get('.text-right').eq(1).should('contain', '$1806.00'); 
+  //   });
+  // });
   
-  Then('the total of the cart should be updated reflecting the new quantity of the product', () => {
-    cy.get('.table-bordered tfoot .text-right').last().should('contain', '$1929.20'); 
-  });
+  // Then('the total of the cart should be updated reflecting the new quantity of the product', () => {
+  //   cy.get('.table-bordered tfoot .text-right').last().should('contain', '$1929.20'); 
+  // });
